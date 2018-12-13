98ec7b15c3eecddeae566ef96a126896b295c353
Schnell <bwschnell52@gmail.com>
2018-12-13 17:38:10 -0600

    Pulling cards from trello

--- trello/App.js
+++ trello/App.js
@@ -6,17 +6,15 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+import React from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import CardScreen from './Screens/CardScreen';
 import LoginScreen from './Screens/LoginScreen';
 import EditCardScreen from './Screens/EditCardScreen';
 import {createStackNavigator, createAppContainer} from "react-navigation";
+import { fetchCards } from "./api";
 
 const AppNavigator = createStackNavigator({
-    Home: {
-        screen: LoginScreen
-    },
     CardScreen: {
         screen: CardScreen
     },
@@ -25,29 +23,44 @@ const AppNavigator = createStackNavigator({
     }
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    const AppContainer = createAppContainer(AppNavigator);
-    return (<AppContainer/>);
-  }
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: [],
+        }
+    }
+
+    componentDidMount() {
+        this.getCards();
+    }
+
+    getCards = async () => {
+        const cards = await fetchCards();
+        this.setState({ cards: cards });
+    }
+
+    render() {
+        const AppContainer = createAppContainer(AppNavigator);
+        return (<AppContainer screenProps={{ cards: this.state.cards }} />);
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
---
+++ trello/api.js
@@ -0,0 +1,22 @@
+export const fetchCards = async () => {
+    try {
+        var cards = [];
+        const response = await fetch('https://api.trello.com/1/boards/lps48Nau/cards?key=d0443a775a55449e7bd587b641633b17&token=d4e246f1179b1c630d28fd3cb404111ca66d02529311efb0d92d13a9428fa880');
+        const json = await response.json();
+        json.forEach(function (e) {
+                cards = [...cards, {"name": e.name, "desc": e.desc}];
+        })
+        return cards;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const processCards = (card) => {
+    return {
+        name: card.name,
+        desc: card.desc
+    }
+}
+
+const addKeys = (val, key) => ({key, ...val});
