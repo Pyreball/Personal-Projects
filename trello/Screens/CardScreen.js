5edc4e2f4ce15509428f447d4fe8c176d12a3b35
Schnell <bwschnell52@gmail.com>
2018-12-13 18:44:48 -0600

    Showing cards from trello

--- trello/Screens/CardScreen.js
+++ trello/Screens/CardScreen.js
@@ -6,56 +6,62 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+import React from 'react';
 import {Platform, StyleSheet, Text, View, ViewPagerAndroid} from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android:
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-    <ViewPagerAndroid
-      style={styles.viewPager}
-      initialPage={0}>
-      <View style={styles.pageStyle} key="1">
-        <Text>First page</Text>
-      </View>
-      <View style={styles.pageStyle} key="2">
-        <Text>Second page</Text>
-      </View>
-    </ViewPagerAndroid>
-  );
-  }
+const Card = props => (
+    <View style={styles.pageStyle}>
+        <Text>{props.card.name}</Text>
+    </View>
+)
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const cards = this.props.screenProps.cards.map((card, key) =>
+            <Card card={card} key={key}/>
+        );
+
+        return (
+            <ViewPagerAndroid style={styles.viewPager} initialPage={0}>
+                {cards}
+            </ViewPagerAndroid>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  viewPager: {
-    flex: 1
-  },
-  pageStyle: {
-    alignItems: 'center',
-    padding: 20,
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    viewPager: {
+        flex: 1
+    },
+    pageStyle: {
+        alignItems: 'center',
+        padding: 20,
+    }
 });
